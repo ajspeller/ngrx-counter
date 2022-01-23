b8956330f4627fd4fa32da9049eabb949e8dfcf6
AJ Speller <ajspeller@gmail.com>
2022-01-23 13:09:16 -0500

    Lazy Loading of feature areas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -14,37 +13,15 @@ import { HeaderComponent } from './shared/components/header/header.component';
 
 import { HomeComponent } from './home/home.component';
 
-import { CounterComponent } from './counter/counter/counter.component';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
-import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
-import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
-
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
-
 import { appReducer } from './store/app.state';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    CounterOutputComponent,
-    CounterButtonsComponent,
-    CustomCounterInputComponent,
-    HomeComponent,
-    HeaderComponent,
-    PostsListComponent,
-    AddPostComponent,
-    EditPostComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
